f1fad73ef8b9c20ecc15f3b6d4ebf45f726310e3
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-07-04 16:18:56 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Navbar from "components/Navbar";
+import ProtectedRoute from "components/ProtectedRoute";
+import { AuthContextProvider } from "context/AuthContext";
+import Account from "pages/Account";
+import Home from "pages/Home";
+import Login from "pages/Login";
+import Signup from "pages/Signup";
+import { Route, Routes } from "react-router-dom";
+
+export const App = () => {
+  return (
+    <>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/account"
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
+    </>
+  );
+};
